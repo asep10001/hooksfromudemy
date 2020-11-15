e58cf64243561d48f592ffcb8d5377a8e8227f3f
asep10001 <a2h2hemingway@gmail.com>
2020-11-15 22:30:11 +0700

    using useRef to take a reference to a specific elemnt and using plain javascript to slect dom element in react

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+        if (ref.current.contains(event.target)){
+            return;
+        }
+        setIsDropdownOpen(false);
+    });
+  }, []);
   const renderedOptions = options.map((option) => {
-      if(option.value === selected.value){
-          return null
-      }
+    if (option.value === selected.value) {
+      return null;
+    }
     return (
-      <div key={option.value} className="item" onClick={()=>onSelectedChange(option)}>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onSelectedChange(option);
+        }}
+      >
         {option.label}
       </div>
     );
   });
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            setIsDropdownOpen(!isDropdownOpen);
+          }}
+          className={`ui selection dropdown ${
+            isDropdownOpen ? "visible active" : ""
+          }`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${isDropdownOpen ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
