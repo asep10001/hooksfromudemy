53b01747fb7a72919a78398f434343e8c2bf6f37
asep10001 <a2h2hemingway@gmail.com>
2020-11-15 21:40:56 +0700

    mapping and filtering the dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Accordion, Dropdown, Search } from "./components";
 
 const items = [
   {
-    title:'What is React?',
-    contents : 'React is a frontend JS framework'
+    title: "What is React?",
+    contents: "React is a frontend JS framework",
   },
   {
-  
-    title: 'Why Use React?',
-    contents: 'React is a favorite JS libray among engineers'
+    title: "Why Use React?",
+    contents: "React is a favorite JS libray among engineers",
   },
   {
-    title: 'How do you use react?',
-    contents: 'Use React by creating components'
-  }
-
-]
-
+    title: "How do you use react?",
+    contents: "Use React by creating components",
+  },
+];
 
 const options = [
   {
-    label : "Red",
-    value : 'red'
+    label: "Red",
+    value: "red",
   },
   {
-    label : "Green",
-    value : 'green'
+    label: "Green",
+    value: "green",
   },
   {
-    label : "Blue",
-    value : 'blue'
+    label: "Blue",
+    value: "blue",
   },
-
-]
+];
 
 const App = () => {
+  const [selected, setselected] = useState(options[0]);
   return (
     <>
       <Accordion items={items} />
       <Search />
-      <Dropdown options={options}/>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setselected}
+        options={options}
+      />
     </>
   );
-}
+};
 
 export default App;
