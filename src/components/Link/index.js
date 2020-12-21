277049d4bb8c824e30e9fd50c0037da12e0f35e3
asep10001 <asep10001@mail.unpad.ac.id>
2020-12-21 20:40:45 +0700

    changing the url

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const onClick = (event) => {
-    event.preventDefault();
-}
+
 const Link = ({ className, href, children }) => {
+    const onClick = (event) => {
+        event.preventDefault();
+        window.history.pushState({}, "", href);
+    }
     return (
-    <a
-    onClick={onClick} className={className} href={href}>{children}</a>)
+        <a
+            onClick={onClick} className={className} href={href}>{children}</a>)
 };
 
 export default Link;
