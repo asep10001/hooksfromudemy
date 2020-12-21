b789c3f87c02dfe6a732f85dd403120fd387bab6
asep10001 <asep10001@mail.unpad.ac.id>
2020-12-21 19:03:04 +0700

    debouncing translations updates

--- src/components/Convert/index.js
+++ src/components/Convert/index.js
@@ -3,11 +3,26 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('')
+    const [debouncedText, setDebouncedText] = useState(text)
+
+    useEffect(() => {
+
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
+
+    
     useEffect(() => {
         const doTranslations = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -17,8 +32,7 @@ const Convert = ({ language, text }) => {
         }
 
         doTranslations();
-
-    }, [language, text])
+    }, [language, debouncedText])
 
     return (
         <div>
